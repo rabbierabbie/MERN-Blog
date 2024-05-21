ef28f955a7a52b4f6146c39e7cc9f8ed3c61fb2d
rabbierabbie <shreya.srivastava.eee22@itbhu.ac.in>
2024-05-21 21:35:16 +0530

    Adding Header Using Flowbite

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,10 +5,13 @@ import Dashboard from './Pages/Dashboard'
 import Projects from './Pages/Projects'
 import Signin from './Pages/Signin'
 import Signup from './Pages/Signup'
+import Header from './Components/Header'
 
 export default function App() {
   return (
     <BrowserRouter>
+      <Header/>
+      {/*this is placed outside the router tag as we want this everywhere in the browser.In our project our header changes upon screen size change. Comments are added this way in JSX*/}
       <Routes>
         <Route path='/' element={<Home />}/>
         <Route path='/about' element={<About />}/>
