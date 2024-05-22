04e6641b4427ab5170b68e1e2859f3df08d2a6ce
rabbierabbie <shreya.srivastava.eee22@itbhu.ac.in>
2024-05-22 13:54:32 +0530

    Adding user model and updating sidenotes

---
+++ api/models/user.model.js
@@ -0,0 +1,24 @@
+import mongoose from 'mongoose';
+
+const userSchema=new mongoose.Schema(
+    { //starts like this
+        username: {
+          type: String,
+          required: true,
+          unique: true,
+        },
+        email: {
+          type: String,
+          required: true,
+          unique: true,
+        },
+        password: {
+          type: String,
+          required: true,
+        }
+    }, //ends like this
+    {timestamps:true}
+);
+const User = mongoose.model('User', userSchema);
+
+export default User;//but only one element can be exported to another component at a time as a default export.
