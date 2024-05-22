4d66186684f9048448c64df886b17f23e841b10a
rabbierabbie <shreya.srivastava.eee22@itbhu.ac.in>
2024-05-22 12:38:17 +0530

    Connected to the Database and created dotenv file

--- api/index.js
+++ api/index.js
@@ -1,7 +1,17 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
 
+dotenv.config();
 const app=express();
+mongoose.connect(process.env.MONGO)
+.then(()=>{
+    console.log('Connected');
+})
+.catch((e)=>{
+    console.log(e);
+});
 
 app.listen(3000,()=>{
-    console.log("Server is running on port 3000");
+    console.log("Server is running on port 3000!!");
 })
