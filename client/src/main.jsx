1cc0652ad1b8abe35123583f68c3a080b46b6841
rabbierabbie <shreya.srivastava.eee22@itbhu.ac.in>
2024-05-23 21:00:47 +0530

    Added redux-persist to ensure that the user is signed in on refresh

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,18 +3,22 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import { store } from './redux/store.js';
+import { store, persistor} from './redux/store.js';
 //Redux Persist handles the Redux store's storage and retrieval automatically, making state persistence and retrieval easier.
 import { Provider } from 'react-redux';
 //You should start by wrapping your entire application in a <Provider> component to make the store available throughout the component tree:
 //The <Provider> component makes the Redux store available to any nested components that need to access the Redux store.
-
+import { PersistGate } from 'redux-persist/integration/react';
 //With the Redux Persist library, developers can save the Redux store in persistent storage, such as local storage. Therefore, even after refreshing the browser, the site state will be preserved. Redux Persist also includes methods that allow us to customize the state that gets persisted and rehydrated, all with an easily understandable API.
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   //createRoot(rootElement) is a React utility function used to create a react root element, which is a DOM element in which UI components render themselves. It takes as a parameter the root element that should be created.
-    <Provider store={store}>
-        <App />
-    </Provider>
+  <PersistGate persistor={persistor}>
+  <Provider store={store}>
+  
+      <App />
+    
+  </Provider>
+</PersistGate>
 );
