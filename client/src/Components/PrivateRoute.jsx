4e4f9d255bf9e47614a9832f1200c98cafdbe716
rabbierabbie <shreya.srivastava.eee22@itbhu.ac.in>
2024-05-25 14:00:03 +0530

    Completed Sidebar of the Dashboard

--- client/src/Components/PrivateRoute.jsx
+++ client/src/Components/PrivateRoute.jsx
@@ -4,4 +4,5 @@ import { Outlet, Navigate } from 'react-router-dom';
 export default function PrivateRoute() {
   const { currentUser } = useSelector((state) => state.user);
   return currentUser ? <Outlet /> : <Navigate to='/signin' />;
+  {/*There are many built in components in React router version 6 and Navigate component is one of them. It is a wrapper for the useNavigate hook, and used to change the current location on rendering it */}
 }
--- client/src/Pages/Dashboard.jsx
+++ client/src/Pages/Dashboard.jsx
@@ -1,7 +1,28 @@
-import React from 'react'
+import {useLocation} from 'react-router-dom'
+import DashProfile from '../Components/DashProfile'
+import DashSidebar from '../Components/DashSidebar'
+import { useEffect, useState } from 'react';
 
 export default function Dashboard() {
+  const location = useLocation();
+  const [tab, setTab] = useState('');
+  useEffect(() => {
+    const urlParams = new URLSearchParams(location.search);
+    const tabFromUrl = urlParams.get('tab');
+    if (tabFromUrl) {
+      setTab(tabFromUrl);
+    }
+  }, [location.search]);
   return (
-    <div>Dashboard</div>
+    <div className='min-h-screen flex flex-col md:flex-row'>
+      <div className='md:w-56'>
+        {/* Sidebar */}
+        <DashSidebar />
+      </div>
+      {/* profile... */}
+      <div>
+        {tab === 'profile' && <DashProfile />}
+      </div>
+    </div>
   )
 }
