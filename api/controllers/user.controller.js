bdb9a69986555b89661a33e92fe42af15661451a
rabbierabbie <shreya.srivastava.eee22@itbhu.ac.in>
2024-05-27 12:52:11 +0530

    Created delete user functionality method

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -61,3 +61,17 @@ export const updateUser = async (req, res, next) => {
     next(error);
   }
 };
+
+export const deleteUser = async (req, res, next) => {
+  if (req.user.id !== req.params.userId) {
+    return next(errorHandler(403, 'You are not allowed to update this user'));
+  }
+  try{
+    await User.findByIdAndDelete(req.user.id);
+    res.status(200).json({'message':'User has been deleted'});
+  }
+  catch(err){
+    nect(err);
+  }
+  
+}
--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import {
+  deleteUser,
     updateUser,
   } from '../controllers/user.controller.js';
   import { verifyToken } from '../utils/verifyUser.js';
@@ -9,5 +10,6 @@ const router=express.Router();
 
 router.get('/test', test);
 router.put('/update/:userId', verifyToken, updateUser);
+router.delete('/delete/:userId',verifyToken,deleteUser);
 
 export default router;
