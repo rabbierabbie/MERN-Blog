9ca501f979c9ba10b973b4581511d5b1ff9b6245
rabbierabbie <shreya.srivastava.eee22@itbhu.ac.in>
2024-05-27 13:55:23 +0530

    Added delete functionality to delete user button

--- client/src/redux/user/userSlice.js
+++ client/src/redux/user/userSlice.js
@@ -1,44 +1,73 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice } from '@reduxjs/toolkit';
 
-const initialState={
-    currentUser:null,
-    error:null,
-    loading:false
-}
-//createSlice-A function that accepts an initial state, an object of reducer functions, and a "slice name", and automatically generates action creators and action types that correspond to the reducers and state.
-const userSlice=createSlice({
-    name:'user',
-    initialState,
-    reducers:{
-        //There are three types of actions to call the APIs. Each action has a specific role — send a request to API with a payload (signInRequest), celebrate successful response (signInSuccess), and handle failures (signInFailure).
-        signInStart:(state)=>{
-            state.loading=true;
-            state.error=null;
-        },
-        signInSuccess:(state,action)=>{
-            state.currentUser=action.payload;
-            state.loading=false;
-            state.error=null;
-        },
-        signInFailure:(state,action)=>{
-            state.loading=false;
-            state.error=action.payload;
-        },
-        updateStart: (state) => {
-            state.loading = true;
-            state.error = null;
-          },
-          updateSuccess: (state, action) => {
-            state.currentUser = action.payload;
-            state.loading = false;
-            state.error = null;
-          },
-          updateFailure: (state, action) => {
-            state.loading = false;
-            state.error = action.payload;
-          },
-}
-})
+const initialState = {
+  currentUser: null,
+  error: null,
+  loading: false,
+};
 
-export const {signInStart,signInSuccess,signInFailure,updateStart,updateFailure,updateSuccess}=userSlice.actions;
+const userSlice = createSlice({
+  name: 'user',
+  initialState,
+  reducers: {
+    signInStart: (state) => {
+      state.loading = true;
+      state.error = null;
+    },
+    signInSuccess: (state, action) => {
+      state.currentUser = action.payload;
+      state.loading = false;
+      state.error = null;
+    },
+    signInFailure: (state, action) => {
+      state.loading = false;
+      state.error = action.payload;
+    },
+    updateStart: (state) => {
+      state.loading = true;
+      state.error = null;
+    },
+    updateSuccess: (state, action) => {
+      state.currentUser = action.payload;
+      state.loading = false;
+      state.error = null;
+    },
+    updateFailure: (state, action) => {
+      state.loading = false;
+      state.error = action.payload;
+    },
+    deleteUserStart: (state) => {
+      state.loading = true;
+      state.error = null;
+    },
+    deleteUserSuccess: (state) => {
+      state.currentUser = null;
+      state.loading = false;
+      state.error = null;
+    },
+    deleteUserFailure: (state, action) => {
+      state.loading = false;
+      state.error = action.payload;
+    },
+    signoutSuccess: (state) => {
+      state.currentUser = null;
+      state.error = null;
+      state.loading = false;
+    },
+  },
+});
+
+export const {
+  signInStart,
+  signInSuccess,
+  signInFailure,
+  updateStart,
+  updateSuccess,
+  updateFailure,
+  deleteUserStart,
+  deleteUserSuccess,
+  deleteUserFailure,
+  signoutSuccess,
+} = userSlice.actions;
+
 export default userSlice.reducer;
