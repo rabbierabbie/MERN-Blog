84d1da4a27270c42a1ea8c8f983f05047e446a23
rabbierabbie <shreya.srivastava.eee22@itbhu.ac.in>
2024-05-28 14:15:17 +0530

    Added life to the delete post button on the user posts page

--- api/routes/post.route.js
+++ api/routes/post.route.js
@@ -1,9 +1,10 @@
 import express from 'express';
 import {verifyToken} from '../utils/verifyUser.js';
-import { create, getposts } from '../controllers/post.controller.js';
+import { create, deletepost, getposts } from '../controllers/post.controller.js';
 
 const router=express.Router();
 
 router.post('/create',verifyToken,create);
 router.get('/getposts',getposts); //no verification of token required as anyone can see the posts.
+router.delete('/deletepost/:postId/:userId',verifyToken,deletepost);
 export default router;
--- client/src/Components/DashPosts.jsx
+++ client/src/Components/DashPosts.jsx
@@ -15,6 +15,8 @@ export default function DashPosts() {
   const { currentUser } = useSelector((state) => state.user);
   const [userPosts, setUserPosts] = useState([]);
   const [showMore, setShowMore] = useState(true);
+  const [postIdToDelete, setPostIdToDelete] = useState('');
+  const [showModal, setShowModal] = useState(false);
 
   useEffect(() => {
     const fetchPosts = async () => {
@@ -23,6 +25,9 @@ export default function DashPosts() {
         const data = await res.json();
         if (res.ok) {
           setUserPosts(data.posts);
+          if (data.posts.length < 9) {
+            setShowMore(false);
+          }
         }
       } catch (error) {
         console.log(error.message);
@@ -45,6 +50,29 @@ export default function DashPosts() {
           if (data.posts.length < 9) {
             setShowMore(false);
           }
+        }
+      } catch (error) {
+        console.log(error.message);
+      }
+    };
+
+    const handleDeletePost = async () => {
+      setShowModal(false);
+      try {
+        const res = await fetch(
+          `/api/post/deletepost/${postIdToDelete}/${currentUser._id}`,
+          {
+            method: 'DELETE',
+          }
+        );
+        const data = await res.json();
+        if (!res.ok) {
+          console.log(data.message);
+        } else {
+          setUserPosts((prev) =>
+            prev.filter((post) => post._id !== postIdToDelete)
+          );
+          
         }
       } catch (error) {
         console.log(error.message);
@@ -96,6 +124,10 @@ export default function DashPosts() {
                   <Table.Cell>
                     <span
                       className='font-medium text-red-500 hover:underline cursor-pointer'
+                      onClick={() => {
+                        setShowModal(true);
+                        setPostIdToDelete(post._id);
+                      }}
                     >
                       Delete
                     </span>
@@ -124,6 +156,30 @@ export default function DashPosts() {
           ):(
             <p>You have no posts yet!</p>
           )}
+          <Modal
+        show={showModal}
+        onClose={() => setShowModal(false)}
+        popup
+        size='md'
+      >
+        <Modal.Header />
+        <Modal.Body>
+          <div className='text-center'>
+            <HiOutlineExclamationCircle className='h-14 w-14 text-gray-400 dark:text-gray-200 mb-4 mx-auto' />
+            <h3 className='mb-5 text-lg text-gray-500 dark:text-gray-400'>
+              Are you sure you want to delete this post?
+            </h3>
+            <div className='flex justify-center gap-4'>
+              <Button color='failure' onClick={handleDeletePost}>
+                Yes, I'm sure
+              </Button>
+              <Button color='gray' onClick={() => setShowModal(false)}>
+                No, cancel
+              </Button>
+            </div>
+          </div>
+        </Modal.Body>
+      </Modal>
     </div>
   );
 }
