b90003fbd88339fe309d4b7f6af12b635d8562da
rabbierabbie <shreya.srivastava.eee22@itbhu.ac.in>
2024-05-29 11:01:12 +0530

    Completed post page layout

--- client/src/Pages/PostPage.jsx
+++ client/src/Pages/PostPage.jsx
@@ -2,12 +2,12 @@ import { Button, Spinner } from 'flowbite-react';
 import { useEffect, useState } from 'react';
 import { Link, useParams } from 'react-router-dom';
 
-
 export default function PostPage() {
   const { postSlug } = useParams();
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(false);
   const [post, setPost] = useState(null);
+  const [recentPosts, setRecentPosts] = useState(null);
 
   useEffect(() => {
     const fetchPost = async () => {
@@ -33,12 +33,65 @@ export default function PostPage() {
     fetchPost();
   }, [postSlug]);
 
+  useEffect(() => {
+    try {
+      const fetchRecentPosts = async () => {
+        const res = await fetch(`/api/post/getposts?limit=3`);
+        const data = await res.json();
+        if (res.ok) {
+          setRecentPosts(data.posts);
+        }
+      };
+      fetchRecentPosts();
+    } catch (error) {
+      console.log(error.message);
+    }
+  }, []);
+
   if (loading)
     return (
       <div className='flex justify-center items-center min-h-screen'>
         <Spinner size='xl' />
       </div>
     );
+     {/*Read about main tag : https://www.w3schools.com/tags/tag_main.asp#:~:text=The%20tag%20specifies%20the,site%20logos%2C%20and%20search%20forms. The <main> tag specifies the main content of a document. The content inside the <main> element should be unique to the document. It should not contain any content that is repeated across documents such as sidebars, navigation links, copyright information, site logos, and search forms.Note: There must not be more than one <main> element in a document. The <main> element must NOT be a descendant of an <article>, <aside>, <footer>, <header>, or <nav> element.*/}
+  return (
+      <main className='p-3 flex flex-col max-w-6xl mx-auto min-h-screen'>
+      <h1 className='text-3xl mt-10 p-3 text-center font-serif max-w-2xl mx-auto lg:text-4xl'>
+        {post && post.title}
+      </h1>
+      <Link
+        to={`/search?category=${post && post.category}`}
+        className='self-center mt-5'
+      >
+        <Button color='gray' pill size='xs'>
+          {post && post.category}
+        </Button>
+      </Link>
+      <img
+        src={post && post.image}
+        alt={post && post.title}
+        className='mt-10 p-3 max-h-[600px] w-full object-cover'
+      />
+      <div className='flex justify-between p-3 border-b border-slate-500 mx-auto w-full max-w-2xl text-xs'>
+        <span>{post && new Date(post.createdAt).toLocaleDateString()}</span>
+      </div>
+      <div
+        className='p-3 max-w-2xl mx-auto w-full post-content'
+        dangerouslySetInnerHTML={{ __html: post && post.content }}
+      ></div>
+     {/*<div className='max-w-4xl mx-auto w-full'>
+        <CallToAction />
+</div>
+<CommentSection postId={post._id} />
 
-  return <div>Hello</div>
+      <div className='flex flex-col justify-center items-center mb-5'>
+        <h1 className='text-xl mt-5'>Recent articles</h1>
+        <div className='flex flex-wrap gap-5 mt-5 justify-center'>
+          {recentPosts &&
+            recentPosts.map((post) => <PostCard key={post._id} post={post} />)}
+        </div>
+          </div>*/}
+    </main>
+  );
 }
