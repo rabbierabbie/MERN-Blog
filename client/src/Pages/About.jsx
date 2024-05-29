39a928602bbc1362f21fe84f60fbc7dd891d1360
rabbierabbie <shreya.srivastava.eee22@itbhu.ac.in>
2024-05-29 11:55:24 +0530

    Added about page

--- client/src/Pages/About.jsx
+++ client/src/Pages/About.jsx
@@ -1,7 +1,27 @@
-import React from 'react'
-
 export default function About() {
   return (
-    <div>About</div>
-  )
+    <div className='min-h-screen flex items-center justify-center'>
+      <div className='max-w-2xl mx-auto p-3 text-center'>
+        <div>
+          <h1 className='text-3xl font font-semibold text-center my-7'>
+            About Shreya's Blog
+          </h1>
+          <div className='text-md text-gray-500 flex flex-col gap-6'>
+            <p>
+              Welcome! This blog was created by Shreya Srivastava
+              as a personal project to share her thoughts and ideas with the
+              world. Shreya is a passionate developer who loves to keep learning about
+              technology, coding and much more.
+            </p>
+
+            <p>
+              On this blog, you'll find weekly articles and tutorials on topics
+              such as web development, software engineering, and programming
+              languages. So keep an eye on the new and fresh content waiting to be served to you!
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
